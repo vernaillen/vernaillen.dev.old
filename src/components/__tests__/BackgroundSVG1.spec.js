54bdc0f275db7a8a547674d704e61759754ebd39
Wouter Vernaillen <wouter@vernaillen.com>
2022-04-10 14:36:06 +0200

    check if variable is injected in dom

--- src/components/__tests__/BackgroundSVG1.spec.js
+++ src/components/__tests__/BackgroundSVG1.spec.js
@@ -5,7 +5,9 @@ import BackgroundSVG1 from "../BackgroundSVG1.vue";
 describe("BackgroundSVG1.vue", () => {
   it("should render", () => {
     const wrapper = mount(BackgroundSVG1);
-
     expect(wrapper.find("svg").exists()).toBe(true);
+    expect(wrapper.vm.stopColor).toBe("#9C8E1B");
+    const stopLine = wrapper.findAll('stop[stop-color="#9C8E1B"]');
+    expect(stopLine.length).toBe(6);
   });
 });
