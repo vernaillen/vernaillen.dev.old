b7ed394b5a2defea0364814b39fd05670b80fb9b
Wouter Vernaillen <wouter@vernaillen.com>
2022-04-10 13:43:59 +0200

    App.spec.js

---
+++ src/__tests__/App.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+import { createHead } from "@vueuse/head";
+import App from "../App.vue";
+
+describe("App.vue", () => {
+  it("should render", () => {
+    const wrapper = shallowMount(App, {
+      global: {
+        plugins: [createHead()]
+      }
+    });
+
+    wrapper.findComponent({ name: "router-view" });
+  });
+});
