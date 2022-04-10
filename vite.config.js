72c51b5b4f4fbe20ea9609b9a7cd9370755de10b
Wouter Vernaillen <wouter@vernaillen.com>
2022-04-10 11:57:52 +0200

    circleci & coverage config

--- vite.config.js
+++ vite.config.js
@@ -54,7 +54,7 @@ export default defineConfig({
     excludeNodeModules: true,
     environment: "jsdom",
     coverage: {
-      all: true,
+      reporter: ['json'],
       include: ["src/**/*.js", "src/**/*.vue"],
     },
     extension: [".js", ".vue"],
