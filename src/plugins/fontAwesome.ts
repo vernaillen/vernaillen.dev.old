82c259937d2fddcf6e11f682881d98d02927eb1a
Wouter Vernaillen <wouter@vernaillen.com>
2022-06-24 16:16:34 +0200

    fix lint errors

--- src/plugins/fontAwesome.ts
+++ src/plugins/fontAwesome.ts
@@ -31,10 +31,10 @@ library.add(
 );
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
 // Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; /* eslint-disable import/first */
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export const install: UserModule = ({ app }) => {
   app.component("font-awesome-icon", FontAwesomeIcon);
