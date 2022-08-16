b6b64eba18769051f0573b0ffdcbfbf339fabc8b
Wouter Vernaillen <wouter@vernaillen.com>
2022-08-16 13:51:32 +0200

    recaptcha autoHideBadge

--- src/plugins/recaptcha.ts
+++ src/plugins/recaptcha.ts
@@ -2,7 +2,14 @@ import { VueReCaptcha } from 'vue-recaptcha-v3'
 import type { UserModule } from './userModule'
 
 export const install: UserModule = ({ app, isClient }) => {
-  if (isClient)
-    app.use(VueReCaptcha, { siteKey: '6LeL37UgAAAAAH0D_-DbYjVUKWkZ7K9G4YUSPLwg' })
+  if (isClient) {
+    app.use(VueReCaptcha, {
+      siteKey: '6LeL37UgAAAAAH0D_-DbYjVUKWkZ7K9G4YUSPLwg',
+      loaderOptions: {
+        useRecaptchaNet: true,
+        autoHideBadge: true,
+      },
+    })
+  }
 }
 
