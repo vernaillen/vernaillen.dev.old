4d37e9e6c46c7f5abdd0e316d16a78ef07da242f
Wouter Vernaillen <wouter@vernaillen.com>
2023-06-14 13:13:17 +0200

    removed server/api/github

--- server/api/github.post.ts
+++
@@ -1,7 +0,0 @@
-import type { GitHubData } from '~~/types/github'
-
-export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-  const url = `https://api.github.com/repos/vernaillen/vernaillen.dev/commits?path=/content/${body.filename}`
-  return await $fetch<GitHubData[]>(url, { query: { page: 1, per_page: 1 } })
-})
