cb79b01f265e50fed4a26ad09997a2e738e011e0
Wouter Vernaillen <wouter@vernaillen.com>
2023-06-20 16:12:04 +0200

    @nuxthq/studio & define CareerStep in nuxt.schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@nuxt/image',
+    '@nuxthq/studio',
     '@nuxtjs/color-mode',
     '@nuxtjs/plausible',
     '@nuxtjs/tailwindcss',
---
+++ nuxt.schema.ts
@@ -0,0 +1,56 @@
+import type { CareerStep } from '~~/types'
+
+export default defineNuxtSchema({
+  appConfig: {
+    longDateFormat: 'DD MMMM YYYY',
+    shortDateFormat: 'DD MMM YYYY',
+    instagram: {
+      enabled: false
+    },
+    careerSteps: [
+      ...CareerStep
+    ],
+    socials: {
+      twitter: {
+        name: 'Twitter',
+        url: '',
+        icon: 'simple-icons:twitter'
+      },
+      github: {
+        name: 'GitHub',
+        url: '',
+        icon: 'simple-icons:github'
+      },
+      facebook: {
+        name: 'Facebook',
+        url: '',
+        icon: 'simple-icons:facebook'
+      },
+      instagram: {
+        name: 'Instagram',
+        url: '',
+        icon: 'simple-icons:instagram'
+      },
+      youtube: {
+        name: 'YouTube',
+        url: '',
+        icon: 'simple-icons:youtube'
+      },
+      medium: {
+        name: 'Medium',
+        url: '',
+        icon: 'simple-icons:medium'
+      },
+      mastodon: {
+        name: 'Mastodon',
+        url: '',
+        icon: 'simple-icons:mastodon'
+      },
+      whatsapp: {
+        name: 'WhatsApp',
+        url: '',
+        icon: 'simple-icons:whatsapp'
+      }
+    }
+  }
+})
