4c13bb54958221a1fc5fdf4104233605ce12e974
Wouter Vernaillen <wouter@vernaillen.com>
2023-07-25 14:13:08 +0200

    short month

--- components/FeedPost.vue
+++ components/FeedPost.vue
@@ -41,7 +41,7 @@ const props = defineProps({
               :datetime="datetime"
               locale="en-UK"
               day="numeric"
-              month="long"
+              month="short"
               year="numeric"
             />
           </a>
