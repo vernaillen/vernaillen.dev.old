d2e31f4dc14abfda24633adb4444b1e603e75a71
Wouter Vernaillen <wouter@vernaillen.com>
2023-07-30 12:32:39 +0200

    show skeleton while fetching or filtering posts

--- components/SocialFeed.vue
+++ components/SocialFeed.vue
@@ -1,26 +1,54 @@
 <script lang="ts" setup>
+import { filter } from 'cypress/types/lodash'
+
 const showReblogs = ref(false)
-const [mastodon] = await Promise.all([
-  $fetch('/_social/mastodon')
-])
-const sortedFeed = [...mastodon].sort(
-  (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-)
+const fetchedFeed = ref()
+const filteredFeed = ref()
+async function fetchPosts () {
+  const [mastodon] = await Promise.all([
+    $fetch('/_social/mastodon')
+  ])
+  fetchedFeed.value = [...mastodon]
+    .sort(
+      (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    )
+  filterPosts()
+}
+function filterPosts () {
+  filteredFeed.value = fetchedFeed.value.filter(item => showReblogs.value || !item.reblogged)
+}
+onMounted(() => {
+  fetchPosts()
+})
+watch(() => showReblogs.value, () => {
+  filteredFeed.value = null
+  setTimeout(() => {
+    filterPosts()
+  }, 300)
+})
 </script>
 
 <template>
-  <div>
+  <div class="w-full">
     <div class="pb-5 -mt-5 text-right">
-      Show reblogs: <UToggle v-model="showReblogs" class="-mb-2" />
+      Show reblogs: <UToggle v-model="showReblogs" class="-mb-[20px]" />
     </div>
-    <FeedPost
-      v-for="item, index in sortedFeed"
-      :key="item.permalink"
-      :post="item"
-      class="slide-enter transition-all transform duration-300 ease-in-out "
-      :class="showReblogs || !item.reblogged ? 'opacity-100 block' : '!opacity-0 hidden'"
-      :style="'--enter-stage:' + index + ';--enter-step:60ms;'"
-    />
+    <div v-if="filteredFeed">
+      <FeedPost
+        v-for="item, index in filteredFeed"
+        :key="item.permalink"
+        :post="item"
+        :style="'--enter-stage:' + index + ';--enter-step:60ms;'"
+      />
+    </div>
+    <div v-else class="w-full slide-enter">
+      <USkeleton class="h-8 w-full mb-10" />
+      <div v-for="items, index in [1, 2]" :key="index" class="space-y-2 mb-5">
+        <USkeleton class="h-4 w-full" />
+        <USkeleton class="h-4 w-[100px]" />
+        <USkeleton class="h-4 w-1/2" />
+      </div>
+    </div>
   </div>
 </template>
 
